29816560725d2d3e4760ceb644cdef4a657ced2d
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-20 10:30:54 +0530

    Implement division handling with exception catch

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 
 public class DivisionHandling {
-    
-        // TODO: Read two integers a and b
-        
-        // TODO: Enclose the division in a try block
-        
-        // TODO: Catch ArithmeticException
-        
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a:");
+        int a=sc.nextInt();
+        System.out.println("enter b:");
+        int b=sc.nextInt();
+
+        try{
+            System.out.println(a/b);
+        }
+        catch(ArithmeticException e){
+            System.out.println("divide by zero error");
+        }
+
+    }
 }

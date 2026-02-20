c00343504edda7bc279a85b54e121090ab49e447
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-20 10:34:45 +0530

    Fix divide by zero error message formatting

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -5,16 +5,14 @@ public class DivisionHandling {
     public static void main(String[] args){
 
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a:");
         int a=sc.nextInt();
-        System.out.println("enter b:");
         int b=sc.nextInt();
 
         try{
             System.out.println(a/b);
         }
         catch(ArithmeticException e){
-            System.out.println("divide by zero error");
+            System.out.println("Divide by zero error");
         }
 
     }
